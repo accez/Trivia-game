f06357261030a5a998af3a4b064058de61ecd344
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-20 14:02:03 +0100

    feat(App): :lipstick: add container with styling
    container where data is going to be displayed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <script setup>
+import QuestionScreenVue from './components/QuestionScreen/QuestionScreen.vue';
+import QuestionScreen from './components/QuestionScreen/QuestionScreen.vue';
 </script>
 
 <template>
-  <div />
+  <div class="container">
+    <QuestionScreen />
+  </div>
 </template>
 
 <style>
+
+body{
+  margin:0px;
+}
+
+.container{
+    width: 60vw;
+    height: 60vh;
+    background-color: #fff;
+}
 #app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #2c3e50;
 }
 </style>
