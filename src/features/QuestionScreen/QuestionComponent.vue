3fefb1bfd619c0aa773dd278b2943556a45c3688
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-21 16:28:24 +0100

    feature: Shuffle answers

--- src/features/QuestionScreen/QuestionComponent.vue
+++ src/features/QuestionScreen/QuestionComponent.vue
@@ -1,23 +1,39 @@
 <script setup>
+import { onMounted, onUpdated, ref } from 'vue';
+const answers = ref([])
 const props = defineProps({
-    question: {
-        type:String,
-        required: true
-    },
-    correct_answer:{
-        type: String,
-        required: true
-    },
-    incorrect_answers:{
-        type: Array,
+    results:{
+        type:Array,
         required:true
+    },
+    currentQuestion:{
+        type:Number,
+        required: true
     }
 })
 
+
+
+const shuffleAnswers = (array, correctAnswer) =>{
+    array.push(correctAnswer)
+    return array.sort( ()=>Math.random()-0.5 );
+}
+
+onMounted(() =>{
+    answers.value.push(shuffleAnswers(props.results[props.currentQuestion].incorrect_answers,props.results[props.currentQuestion].correct_answer))
+})
+
+
 </script>
+
 <template>
-  <div />
-  <button />
+  <h3>{{ props.results[currentQuestion].question }}</h3>
+  <button
+    v-for="(answer, i) in answers[0]"
+    :key="i"
+  >
+    {{ answer }}
+  </button>
 </template>
 
 <style scoped>
