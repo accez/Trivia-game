d544b2a415e844b4accf1039c26f7dce03dc8730
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-24 09:11:44 +0100

    style: add 2 indent rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
-    extends: [
-        'plugin:vue/vue3-recommended',
-    ],
-    rules: {
-        "vue/prop-name-casing": ["camelCase" | "snake_case"]
-    }
+ extends: [
+  'plugin:vue/vue3-recommended',
+ ],
+ rules: {
+  "vue/prop-name-casing": ["camelCase" | "snake_case"],
+  "indent": ["error", 1]
+
+ }
 }
