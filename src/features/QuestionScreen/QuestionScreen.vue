91db330b2129c2ce90f72b9745614af773de7acc
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-24 21:03:56 +0100

    feat: Now renders from api

--- src/features/QuestionScreen/QuestionScreen.vue
+++ src/features/QuestionScreen/QuestionScreen.vue
@@ -1,47 +1,25 @@
 <script setup>
 import QuestionComponent from './QuestionComponent.vue';
-import {onBeforeMount, reactive,ref } from 'vue';
+import {onBeforeMount,ref } from 'vue';
+
+const props = defineProps({
+  questionData:{
+    type: Object,
+    required:true
+  }
+});
+
 const currentQuestion = ref(0);
 const answers = ref([]);
-const questions = reactive({
-  results: [
-    {
-      question:"In the book &quot;The Martian&quot;, how long was Mark Watney trapped on Mars (in Sols)?",
-      correct_answer: "549 Days",
-      incorrect_answers: [
-        "765 Days",
-        "401 Days",
-        "324 Days"
-      ]
-    },
-    {
-      question:"Which of these elements on the Periodic Table is a Noble Gas?",
-      correct_answer:"Neon",
-      incorrect_answers: [
-        "Potassium",
-        "Iodine",
-        "Colbalt"
-      ]
-    },{
-      question:"What bird is born with claws on its wing digits?",
-      correct_answer:"Hoatzin",
-      incorrect_answers: [
-        "Cormorant",
-        "Cassowary",
-        "Secretary bird"
-      ]
-    }
-  ]
-});
 
 const addAnswersToArray = (array, incorrectAnswers, correctAnswers) =>{
   return array.value = [...incorrectAnswers,correctAnswers];
 };
 
 const changeQuestion = () =>{
-  if(currentQuestion.value + 1 < questions.results.length){
+  if(currentQuestion.value + 1 < props.questionData.results.length){
     currentQuestion.value++;
-    let changedArray = addAnswersToArray(answers.value,questions.results[currentQuestion.value].incorrect_answers,questions.results[currentQuestion.value].correct_answer);
+    let changedArray = addAnswersToArray(answers.value,props.questionData.results[currentQuestion.value].incorrect_answers,props.questionData.results[currentQuestion.value].correct_answer);
     shuffleAnswers(changedArray);
   }
 };
@@ -51,11 +29,11 @@ const emit = () =>{
 };
 
 const shuffleAnswers = (array) =>{
-  return array.sort( ()=>Math.random()-0.5 );
+  return array.sort( () => Math.random() - 0.5);
 };
 
 onBeforeMount(() =>{
-  let mountedArray = addAnswersToArray(answers.value,questions.results[currentQuestion.value].incorrect_answers,questions.results[currentQuestion.value].correct_answer);
+  let mountedArray = addAnswersToArray(answers.value,props.questionData.results[currentQuestion.value].incorrect_answers,props.questionData.results[currentQuestion.value].correct_answer);
   shuffleAnswers(mountedArray);
 });
 
@@ -64,7 +42,7 @@ onBeforeMount(() =>{
 <template>
   <QuestionComponent
     :answers="answers"
-    :question="questions.results[currentQuestion].question"
+    :question="props.questionData.results[currentQuestion].question"
     @clicked="emit"
   />
 </template>
