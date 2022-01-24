06ae0e5a12c2d80d3652a59e3881e214b52d03be
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-24 09:14:09 +0100

    style: Fix indentation

--- src/features/QuestionScreen/QuestionScreen.vue
+++ src/features/QuestionScreen/QuestionScreen.vue
@@ -1,43 +1,43 @@
 <script setup>
-  import QuestionComponent from './QuestionComponent.vue';
-  import {reactive,ref } from 'vue';
-    const currentQuestion = ref(0)
-    const questions = reactive({
-      results: [
-        {
-          question:"In the book &quot;The Martian&quot;, how long was Mark Watney trapped on Mars (in Sols)?",
-          correct_answer: "549 Days",
-          incorrect_answers: [
-            "765 Days",
-            "401 Days",
-            "324 Days"
-          ]
-        },
-        {
-          question:"Which of these elements on the Periodic Table is a Noble Gas?",
-          correct_answer:"Neon",
-          incorrect_answers: [
-            "Potassium",
-            "Iodine",
-            "Colbalt"
-          ]
-        },{
-          question:"What bird is born with claws on its wing digits?",
-          correct_answer:"Hoatzin",
-          incorrect_answers: [
-          "Cormorant",
-          "Cassowary",
-          "Secretary bird"
-          ]
-        }
-      ]
-  })
-
-  const changeQuestion = () =>{
-    if(currentQuestion.value + 1 < questions.results.length){
-      currentQuestion.value++
-    }
+import QuestionComponent from './QuestionComponent.vue';
+import {reactive,ref } from 'vue';
+const currentQuestion = ref(0)
+const questions = reactive({
+ results: [
+  {
+   question:"In the book &quot;The Martian&quot;, how long was Mark Watney trapped on Mars (in Sols)?",
+   correct_answer: "549 Days",
+   incorrect_answers: [
+    "765 Days",
+    "401 Days",
+    "324 Days"
+   ]
+  },
+  {
+   question:"Which of these elements on the Periodic Table is a Noble Gas?",
+   correct_answer:"Neon",
+   incorrect_answers: [
+    "Potassium",
+    "Iodine",
+    "Colbalt"
+   ]
+  },{
+   question:"What bird is born with claws on its wing digits?",
+   correct_answer:"Hoatzin",
+   incorrect_answers: [
+    "Cormorant",
+    "Cassowary",
+    "Secretary bird"
+   ]
   }
+ ]
+})
+
+const changeQuestion = () =>{
+ if(currentQuestion.value + 1 < questions.results.length){
+  currentQuestion.value++
+ }
+}
 
 </script>
 
