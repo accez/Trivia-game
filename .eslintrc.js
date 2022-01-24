be191172263971e1d8cfea63b64f59c2258c940b
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-24 09:19:45 +0100

    fix: eslint rule now properly use 2 indendt

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
- extends: [
-  'plugin:vue/vue3-recommended',
- ],
- rules: {
-  "vue/prop-name-casing": ["camelCase" | "snake_case"],
-  "indent": ["error", 1]
+  extends: [
+    'plugin:vue/vue3-recommended',
+  ],
+  rules: {
+    "vue/prop-name-casing": ["camelCase" | "snake_case"],
+    "indent": ["error", 2]
 
- }
+  }
 }
